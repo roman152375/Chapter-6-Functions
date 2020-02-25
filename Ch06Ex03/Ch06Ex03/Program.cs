24945eeae161c17e464db8f3900842487134e952
Roman <ratul152375@gmail.com>
2020-02-25 18:55:07 -0500

    ex3

---
+++ Ch06Ex03/Ch06Ex03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch06Ex03
+{
+    class Program
+    {
+        static int sumVals(params int[] vals)
+        {
+            int sum = 0;
+            foreach (int val in vals)
+            {
+                sum += val;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            int sum = sumVals(1, 5, 2, 9, 8);
+            Console.WriteLine("Summed Values = {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
